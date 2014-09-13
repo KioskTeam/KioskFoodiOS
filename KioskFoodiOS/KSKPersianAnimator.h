3f76b2f0fc42d2a3134a68545a1600d29354ee66
taesiri <taesiri@live.com>
2014-09-13 23:38:34 +0430

    Added Persian Animator

---
+++ KioskFoodiOS/KSKPersianAnimator.h
@@ -0,0 +1,13 @@
+//
+//  KSKPersianAnimator.h
+//  KioskFoodiOS
+//
+//  Created by Mohmmad Reza Taesiri on 9/13/14.
+//  Copyright (c) 2014 Kiosk Team. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KSKPersianAnimator : NSObject <UIViewControllerAnimatedTransitioning>
+
+@end
